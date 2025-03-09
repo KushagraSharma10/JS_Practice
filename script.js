ce45654201fbed4577a555e43338c08920dc9242
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-03-09 23:09:24 +0530

    Question 53 is added with solution

--- script.js
+++ script.js
@@ -612,3 +612,17 @@
 // }
 
 // console.log(isSorted([1, 2, 3, 4, 5])) // true
+
+// Question 53. write a javascript program to find the largest even number from an array of integers.
+
+// function FindLargestEven(arr){
+//     let largest = arr[0];
+//     for(let i = 1; i < arr.length; i++){
+//         if(arr[i] % 2 === 0 && arr[i] > largest){
+//             largest = arr[i];
+//         }
+//     }
+//     return largest
+// }
+
+// console.log(FindLargestEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 10
