ff325e2cb978547f881f61fd5d24880745c929e1
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-03-31 00:18:22 +0530

    Question no. 64 is added

---
+++ EvenOrOdd.js
@@ -0,0 +1,10 @@
+
+// Question 64. write a function that tells if a number is even or odd
+
+
+function EvenOrOdd(num){
+    return num % 2 === 0 ? "Even" : "Odd"; 
+}
+
+console.log(EvenOrOdd(5)) // "Odd"
+console.log(EvenOrOdd(2)) // "Odd"
--- script.js
+++ script.js
@@ -765,13 +765,3 @@
 
 
 // back to basic questions 
-
-// Question 64. write a function that tells if a number is even or odd
-
-
-// function EvenOrOdd(num){
-//     return num % 2 === 0 ? "Even" : "Odd"; 
-// }
-
-// console.log(EvenOrOdd(5)) // "Odd"
-// console.log(EvenOrOdd(2)) // "Odd"
